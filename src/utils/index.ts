215974bc6d35574d5d9efd5d8d06764fe0da7017
Judge Godwins <judgegodwins@gmail.com>
2022-08-13 10:01:54 +0100

    use 100 words for randomwordsdb

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,9 @@ export * from './pagination';
 export const bcryptHash = (data: string) => {
   return bcrypt.hash(data, 12);
 }
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
